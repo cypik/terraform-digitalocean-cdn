22f9c59ac16fdf4822fb29cc221ac5d4bffe9933
opzbaldev <baldev@opz0.io>
2023-11-06 18:22:19 +0530

    add github readme.md license update repo

--- example/complete/example.tf
+++ example/complete/example.tf
@@ -6,7 +6,7 @@ provider "digitalocean" {
 ## spaces module call
 ##------------------------------------------------
 module "spaces" {
-  source        = "git::git@github.com:thebaldev/digitalocean-spaces.git"
+  source        = "git::https://github.com/opz0/terraform-digitalocean-spaces.git?ref=v1.0.0"
   name          = "classmet-rest"
   acl           = "private"
   force_destroy = false
