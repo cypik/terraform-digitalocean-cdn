0ccf98aa1df329648f177120e461a68eaed9079e
cypikbaldev <baldev@opz0.io>
2023-11-27 10:44:15 +0530

    update org name

--- example/complete/example.tf
+++ example/complete/example.tf
@@ -6,7 +6,7 @@ provider "digitalocean" {
 ## spaces module call
 ##------------------------------------------------
 module "spaces" {
-  source        = "git::https://github.com/opz0/terraform-digitalocean-spaces.git?ref=v1.0.0"
+  source        = "git::https://github.com/cypik/terraform-digitalocean-spaces.git?ref=v1.0.0"
   name          = "classmet-rest"
   acl           = "private"
   force_destroy = false
